986e95edf9a4587da1bb33f966fdcb1b3b3bb945
Vijay <vijaythevirtue@gmail.com>
2021-05-14 00:06:53 +0530

    ui refactor

--- lib/Screens/HomePage/Widgets/2Body.dart
+++ lib/Screens/HomePage/Widgets/2Body.dart
@@ -3,37 +3,35 @@ import 'package:flutter/cupertino.dart';
 import 'package:furniture_app/Declarations/Images/ImageFiles.dart';
 import 'package:furniture_app/Declarations/Global/Colors/Colors.dart';
 
-Widget buildBody(BuildContext context) => Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        width: double.infinity,
-        height: MediaQuery.of(context).size.height / 1.5,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(35.00),
-              topRight: Radius.circular(35.00),
-            ),
-            color: containerColor),
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              top: -175,
-              child: Container(
-                width: 275,
-                height: 393,
-                child: Image.asset(
-                  chair[0],
-                  fit: BoxFit.contain,
+Widget buildBody(BuildContext context) => Container(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 1.5,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35.00),
+                  topRight: Radius.circular(35.00),
                 ),
+                color: containerColor,
               ),
             ),
-              Text("asakjbdkj"),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text("asakjbdkj")],
-            )
-          ],
-        ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height -
+                (MediaQuery.of(context).size.height - 15),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.5,
+              height: 393,
+              child: Image.asset(
+                chair[0],
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
       ),
     );
