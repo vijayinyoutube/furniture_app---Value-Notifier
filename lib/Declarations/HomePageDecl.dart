f69238bcfe61b121d3d4727c523d83b15bfa67a9
Vijay <vijaythevirtue@gmail.com>
2021-05-30 16:53:25 +0530

    refactor

--- lib/Declarations/HomePageDecl.dart
+++ lib/Declarations/HomePageDecl.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePageDecl with ChangeNotifier {
-  ValueNotifier<int> amount = ValueNotifier<int>(75);
+  int amount=75;
   ValueNotifier<int> count = ValueNotifier<int>(1);
 }
 
--- lib/Screens/HomePage/Widgets/4BottomContainer.dart
+++ lib/Screens/HomePage/Widgets/4BottomContainer.dart
@@ -150,7 +150,7 @@ Widget buildPrice(BuildContext context) => Container(
             text: TextSpan(
               children: <TextSpan>[
                 TextSpan(
-                  text: homepagedecl.amount.value.toString(),
+                  text: homepagedecl.amount.toString(),
                   style: fonts[globalfontIndex](
                     color: primaryColor,
                     fontWeight: FontWeight.bold,
@@ -211,7 +211,7 @@ handleIncDec(bool canInc) {
       : homepagedecl.count.value - 1 >= 1
       ? homepagedecl.count.value -= 1
       : homepagedecl.count.value;
-  homepagedecl.amount.value = homepagedecl.count.value * 75;
+  homepagedecl.amount = homepagedecl.count.value * 75;
 }
 
 Widget buildColorIcons(BuildContext context) => Row(
